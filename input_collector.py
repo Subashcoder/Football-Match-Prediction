96a455c86ca8c745cb8d8187a7f234c7e1a3cfb5
Subash_Rokka <63716286+Subashcoder@users.noreply.github.com>
2024-04-14 00:45:32 -0400

    Adding the input_collector.py file to collect the data for input

--- input_collector.py
+++ input_collector.py
@@ -9,7 +9,7 @@ def file_load():
     # File paths for CSV files
     url = r'C:\Drive D\Downloads\Lambton College\Semester 2\AI\Project\Football_Match_Prediction\Football-Match-Prediction\Data\data\2023-24\gws\gw28.csv'
     url2 = r'C:\Drive D\Downloads\Lambton College\Semester 2\AI\Project\Football_Match_Prediction\Football-Match-Prediction\Data\data\2023-24\player_idlist.csv'
-
+    url3 = r'C:\Drive D\Downloads\Lambton College\Semester 2\AI\Project\Football_Match_Prediction\Football-Match-Prediction\Data\data\2023-24\teams.csv'
     # Player Name and Player Team
     df_player = pd.read_csv(url)
     df_player = df_player[['name', 'team']].drop_duplicates()
@@ -17,6 +17,9 @@ def file_load():
     # Player name and palyer ID
     df_player_id = pd.read_csv(url2)
     df_player_id['name'] = df_player_id['first_name'] + ' ' + df_player_id['second_name']
+    
+    df_team_id = pd.read_csv(url3)
+    df_team_id = df_team_id[['id', 'name']]
 
     # Select only the 'name' and 'id' columns and remove duplicates
     df_player_id = df_player_id[['name', 'id']].drop_duplicates()
@@ -25,7 +28,64 @@ def file_load():
 
     # Merge the two dataframes on 'name' to associate player IDs with player names and teams
     merged_df = pd.merge(df_player, df_player_id, on='name', how='inner')
-    return merged_df
+    return merged_df,df_team_id
+
+
+def expected_goal_involvements(Previous):
+    # Calculate expected goal involvements based on data from the previous rows
+    sum_goals_scored = Previous['goals_scored'].sum()
+    sum_target_missed = Previous['target_missed'].sum()
+    sum_assist = Previous['assists'].sum()
+    sum_big_chance = Previous['big_chances_created'].sum()
+    sum_key_passes = Previous['key_passes'].sum()
+
+    Creativity = Previous['creativity'].mean()
+    influance = Previous['influence'].mean()
+    
+    if (sum_goals_scored + sum_goals_scored + sum_target_missed + sum_assist + sum_big_chance + sum_key_passes ).all() != 0:
+        # Calculate expected goal involvements using aggregated data from previous rows
+        return ((sum_goals_scored / (sum_goals_scored + sum_goals_scored + sum_target_missed + sum_assist + sum_big_chance + sum_key_passes )) * (Creativity / 100) * (influance/ 100)) / 3
+    else:
+        return ((sum_goals_scored ) * (Creativity / 100) * (influance/ 100)) / 3
+    
+
+     
+def expected_goals(Previous):
+    sum_goals = Previous['goals_scored'].sum()
+    threat = Previous['threat'].mean()
+    return (sum_goals * (threat / 100)) / 3
+  
+  
+    
+def expected_goals_conceded(Previous):
+    
+    sum_goal_conceded = Previous['goals_conceded'].sum()
+    error_leading_to_goal = Previous['errors_leading_to_goal']
+    sum_error_leading_to_goal_attempt = Previous['errors_leading_to_goal']
+    
+    if (error_leading_to_goal + sum_error_leading_to_goal_attempt).all() != 0:
+        expected_goal_conceded = sum_goal_conceded / (error_leading_to_goal + sum_error_leading_to_goal_attempt)
+        return expected_goal_conceded / 3
+    else:
+        return sum_goal_conceded / 3
+    
+    
+
+def expected_assists(Previous):
+    Sum_Assists = Previous['assists'].sum()
+    Sum_Open_play_crosses = Previous['open_play_crosses'].sum()
+    key_passes = Previous['key_passes'].sum()
+    ea_index = Previous['ea_index'].mean()
+    
+    if (Sum_Open_play_crosses + key_passes).all() != 0:
+    
+        xA = (Sum_Assists / (Sum_Open_play_crosses + key_passes)) * (ea_index / 100)
+        return xA / 3
+    
+    else:
+        return (Sum_Assists * (ea_index / 100)) / 3
+    
+
 
 
 def get_individual_player_data(player_id):
@@ -49,8 +109,11 @@ def get_individual_player_data(player_id):
     data.sort_values(by='kickoff_time', ascending=False, inplace=True)
     return data.head(3)
 
+def player_stats_calculator():
+    ...
 
-def get_fixtures_data():
+
+def get_fixtures_data(event):
     """ Retrieve the fixtures data for the season
     """
     url = "https://fantasy.premierleague.com/api/fixtures/"
@@ -63,4 +126,17 @@ def get_fixtures_data():
     if response.status_code != 200:
         raise Exception("Response was code " + str(response.status_code))
     data = json.loads(response.text)
+    
+    data = pd.DataFrame(data)
+    _, df_id = file_load()
+    data = data.loc[(data['event'] == event), ['kickoff_time', 'team_h', 'team_a']]
+    id_to_name_map = dict(zip(df_id['id'], df_id['name']))
+
+    # Use map to apply the mapping to team_a and create team_name column
+    data['team_a_name'] = data['team_a'].map(id_to_name_map)
+    data['team_h_name'] = data['team_h'].map(id_to_name_map)
+    
     return data
+
+def Home_team_stats():
+    df = 
